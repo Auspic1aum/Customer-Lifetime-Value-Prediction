4d4967041249f7c558cc1cfa557ae0c1e0c0cd10
Auspic1aum <shubhampapal13@gmail.com>
2025-01-01 17:24:44 +0530

    Requirements and setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="CLV-Prediction",
+    version="0.0.0",
+    author="shubham",
+    author_email="shubhampapal13@gmail.com",
+    packages=find_packages()
+)
